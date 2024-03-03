c9e484981a174e5c12f9ddf7c449d22b61d9499d
Vanshil Patel <vanshilpatel12@gmail.com>
2024-03-03 08:54:38 +0530

    Add account schema with ref to Users table

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,5 @@
+import { Schema } from 'zod';
+
 const mongoose = require('mongoose');
 
 
@@ -13,12 +15,44 @@ mongoose.connect(uri);
 
 
 const UserSchema  = new mongoose.Schema({
-    username : String,
-    password  :String,
-    firstName: String,
-    lastName: String
+    username :{
+        type: String,
+        required : true,
+        minLength: 3,
+        maxLength: 30,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password  :{
+        type: String,
+        required : true,
+        minLength: 8,
+
+    },
+    firstName:{
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
 })
 
 
 
+const AccountSchema = new mongoose.Schema({
+    userId : {type : mongoose.Schema.Types.ObjectId , ref: "User", required: true},
+    balance :{type : Number , required: true}
+
+})
+
 export const User = mongoose.model('User', UserSchema )
+export const Account = mongoose.model('Account', AccountSchema)
+
